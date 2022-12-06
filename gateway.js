cec73e67429065be7b1cd31da0bff0d61f54b4d2
s4j4d <mmmsms97@gmail.com>
2022-12-06 17:01:52 +0330

    completed functionalities for getting bicyles list

--- gateway.js
+++ gateway.js
@@ -1,44 +1,75 @@
 require('dotenv').config();
 const amqplib = require('amqplib');
+const rabbitConnection = require('./utils');
 
 (async()=>{
     try{
     const input = process.argv.slice(2)
     const option = input[0]
     const usernamePassword = input.slice(1,3)
-    console.log('gateway running ....');
+    const routingKeysForPublish = ['register' , 'login', 'bike-request','error','logger' , 'bicycles-list']
+    const routingKeys = ['register-answer' , 'login-answer']
+    const correlationId = generateUuid()
+
+    
+
+    console.log('service running ....');
     const connection = await amqplib.connect()
     const channel = await connection.createChannel()
     const exchange = await channel.assertExchange(process.env.EXCHANGE_NAME , process.env.EXCHANGE_TYPE)
-    const user_queue = await channel.assertQueue('gateway' , {durable:true})
-    const routingKeys = ['register' , 'login', 'logger-service','bicycle-service']
-    const correlationId = generateUuid()
+    const user_queue = await channel.assertQueue(queueName , {durable:true})
     for(const item of routingKeys)
         await channel.bindQueue(user_queue.queue , exchange.exchange , item)
 
-    if(option ==='register'){
-        channel.publish(exchange.exchange , routingKeys[0] , Buffer.from(JSON.stringify(usernamePassword)),{
+    if(option === 'bicycles-list'){
+        channel.publish(exchange.exchange , routingKeysForPublish[5] ,{
             correlationId:correlationId , 
             persistent:true
         })
+    }else{
+        if(option ==='register'){
+            channel.publish(exchange.exchange , routingKeysForPublish[0] , Buffer.from(JSON.stringify(usernamePassword)),{
+                correlationId:correlationId , 
+                persistent:true
+            })
+        }
+        else{
+            channel.publish(exchange.exchange , routingKeysForPublish[1] , Buffer.from(JSON.stringify(usernamePassword)),{
+                correlationId:correlationId , 
+                persistent:true
+            })
+        }
     }
-    else{
-        channel.publish(exchange.exchange , routingKeys[1] , Buffer.from(JSON.stringify(usernamePassword)),{
-            correlationId:correlationId , 
-            persistent:true
-        })
-        channel.consume(user_queue.queue,async (msg)=>{
-                const content = JSON.parse(msg.content)
-                console.log(JSON.parse(msg.content));
-            if(content){
+    channel.consume(user_queue.queue,async (msg)=>{
+        if(msg.content){
+            if(msg.fields.routingKey === 'register-answer'){
+                console.log(msg.content.toString());
             }
-        },{noAck:true})
-    }
+            else if(msg.fields.routingKey === 'login-answer'){
+                const content = JSON.parse(msg.content)
+                if(content){
+                    channel.publish(exchange.exchange , routingKeysForPublish[1] , Buffer.from(JSON.stringify(usernamePassword)),{
+                        correlationId:correlationId , 
+                        persistent:true
+                    })
+                }
+                else if(msg.fields.routingKey === 'bicycles-list'){
+                    console.log(content);
 
-    setTimeout(()=>{
-        connection.close()
-        process.exit(0)
-    },1000)
+                }else{
+                    console.log('not authorized');
+                }
+            }
+            else{}
+        }
+
+setTimeout(()=>{
+    connection.close()
+    process.exit(0)
+},1000)
+    },{noAck:true})
+
+
 }catch(error){
     console.log(error.message);
 }
